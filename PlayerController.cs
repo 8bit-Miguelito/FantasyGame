917ffa1b1e7705632b6691170e91a7e7fd263be6
Miguel Machuca <mmachuca09@outlook.com>
2025-05-05 14:24:48 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,7 +10,7 @@ public class PlayerController : MonoBehaviour
 {
     public float MovementSpeed = 5f;
     public float rotationSpeed = 500f;
-    public float jumpForce = 7f;
+    public float jumpForce = 1.0f;
     public float MaxJumpTime = 0.5f;
 
     public float groundCheckRadius = 0.2f;
@@ -35,39 +36,57 @@ public class PlayerController : MonoBehaviour
         cameraCont = Camera.main.GetComponent<CameraFollow>();
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
+
     } 
     // Update is called once per frame
     private void Update()
     {   
-        float h = Input.GetAxis("Horizontal"); //Movement input from A/D
-        float v = Input.GetAxis("Vertical"); //Movement input from W/S
-        // Check if moving and how much movement. Clamp between 0-1 for Blend Tree in animation
-        float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Math.Abs(v));
-        /*
-        store moveInput in normalized vector (prevents faster movement during diagonal movement)
-        i.e maginute will remain 1 in all directions. 
-        moveDir rotates the vector in 3D plane and stores that direction.
-        */
-        var moveInput = (new Vector3(h,0,v)).normalized;  
-        var moveDir = cameraCont.PlanarRotation * moveInput;
+        float moveAmount = 0f;
+        Vector3 moveDir = Vector3.zero;
+
+        CalculateMove(ref moveAmount, ref moveDir);
     
         //calculate velocity and velocity.y
-        var velocity = moveDir * MovementSpeed;
-        velocity.y = VerticalVelocity();
+        var velocity = CalculateVelocity(in moveDir);
 
         if (Input.GetKey(KeyCode.LeftShift)) {
-            characterController.Move(velocity * 2 * Time.deltaTime);
+            velocity *= 2;
             animator.SetFloat("MovementSpeed", moveAmount / 1, 0.5f, Time.deltaTime);
         } else {
-            characterController.Move(velocity * Time.deltaTime);
             animator.SetFloat("MovementSpeed", moveAmount / 2, 0.5f, Time.deltaTime);
         }
+
+        characterController.Move(velocity * Time.deltaTime);
         
         if (moveAmount > 0) {
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void CalculateMove(ref float moveAmount, ref Vector3 moveDir) {
+        float h = Input.GetAxis("Horizontal"); //Movement input from A/D
+        float v = Input.GetAxis("Vertical"); //Movement input from W/S
+        // Check if moving and how much movement. Clamp between 0-1 for Blend Tree in animation
+        moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Math.Abs(v));
+
+        /*
+        store moveInput in normalized vector (prevents faster movement during diagonal movement)
+        i.e maginute will remain 1 in all directions. 
+        moveDir rotates the vector in 3D plane and stores that direction.
+        */
+        var moveInput = (new Vector3(h,0,v)).normalized;  
+        moveDir = cameraCont.PlanarRotation * moveInput;
+    }
+
+    //calculate velocity and velocity.y
+    //return velocity
+    private Vector3 CalculateVelocity(in Vector3 moveDir) {
+        Vector3 velocity = moveDir * MovementSpeed;
+        velocity.y = VerticalVelocity();
+
+        return velocity;
     }
 
     private float VerticalVelocity() {
